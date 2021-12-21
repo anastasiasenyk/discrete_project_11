29b8543899f091df8668834dad3afde4b1b85daa
Olesia <91982071+OlesiaOmelchuk@users.noreply.github.com>
2021-12-21 16:27:13 +0200

    Create main_project.py

--- main_project.py
+++ main_project.py
@@ -78,21 +78,22 @@ def bipartite(graph_dict: dict) -> bool:
     first_part, second_part = [first_vert],  []
     while queue:
         vertex = queue.pop(0)
-        for neighbour in graph_dict[vertex]:
-            if neighbour not in visited:
-                visited.append(neighbour)
-                queue.append(neighbour)
+        if vertex in list(graph_dict.keys()):
+            for neighbour in graph_dict[vertex]:
+                if neighbour not in visited:
+                    visited.append(neighbour)
+                    queue.append(neighbour)
 
-            if vertex in first_part:
-                if neighbour in first_part:
-                    return False
+                if vertex in first_part:
+                    if neighbour in first_part:
+                        return False
+                    else:
+                        second_part.append(neighbour)
                 else:
-                    second_part.append(neighbour)
-            else:
-                if neighbour in second_part:
-                    return False
-                else:
-                    first_part.append(neighbour)
+                    if neighbour in second_part:
+                        return False
+                    else:
+                        first_part.append(neighbour)
     return True
 
 def is_euler_possible_not_directed(graph_dict: dict) -> bool:
@@ -245,6 +246,85 @@ def hamiltonian_cycle(graph: dict, visited: list, path: list, v: int, length: in
     return False
 
 
+def color_check(color: str, coloring: dict, neighbours: list) -> bool:
+    """
+    Checks whether adjacent vertices are colored in the same color or not.
+    Args:
+        color: color in which we try to color the vertice
+        coloring: dict where keys are vertices of the graph, values - color of that vertice
+        neighbours: list of adjacent vertices
+    Returns:
+        bool: True if the vertice can be colored in that color, False if not
+    >>> color_check('red', {'1': 'blue', '2': 0}, ['1'])
+    True
+    >>> color_check('red', {'1': 'red', '2': 0}, ['1'])
+    False
+    """
+    for neighbour in neighbours:
+        if coloring[neighbour] == color:
+            return False
+    return True
+
+
+def coloring_rec(coloring: dict, vertices: list, vert_index: int, graph: dict) -> bool:
+    """
+    Recursive function, which colors vertices of the graph.
+    Args:
+        coloring: dict where keys are vertices of the graph, values - color of that vertice
+        vertices: list of vertices
+        vert_index: index of vertice in a list vertices
+        graph: dict of vertices
+    Returns:
+        bool: True if graph can be colored in three colors, False if not
+    >>> coloring_rec({'1': 0, '2': 0, '3': 0, '4': 0, '5': 0, '6': 0}, ['1', '2', '3', '4', '5', '6'], 0,{'1': ['2', '5', '4'], '2': ['1', '5', '3'], '3': \
+    ['2', '6', '4'], '4': ['3', '6', '1'], '5': ['2', '1', '6'], '6': ['5', '3', '4']})
+    True
+
+    >>> coloring_rec({'1': 0, '2': 0, '3': 0, '4': 0, '5': 0, '6': 0}, ['1', '2', '3', '4', '5', '6'], 0,{'1': ['2', '3','5', '4','6'], '2': ['1', '5', '3'], '3': \
+    ['2', '6', '4','1'], '4': ['3', '6', '1'], '5': ['2', '1', '6'], '6': ['1','5', '3', '4']})
+    False
+    """
+    if vert_index == len(vertices):
+        return True
+    for color in ['red', 'blue', 'green']:
+        if color_check(color, coloring, graph[vertices[vert_index]]):
+            coloring[vertices[vert_index]] = color
+            if coloring_rec(coloring, vertices, vert_index + 1,  graph):
+                return True
+            coloring[vertices[vert_index]] = 0
+    return False
+
+
+def coloring_decision(graph: dict):
+    """
+    Function which tries to color graph in three colors.
+    Args:
+        graph: dict of vertices
+    Returns:
+        list: list of tuples (vertice, color)
+        str: message that coloring isn't possible
+    >>> coloring_decision({'1': ['2', '5', '4'], '2': ['1', '5', '3'], '3': \
+    ['2', '6', '4'], '4': ['3', '6', '1'], '5': ['2', '1', '6'], '6': ['5', '3', '4']})
+    [('1', 'red'), ('2', 'blue'), ('3', 'red'), ('4', 'green'), ('5', 'green'), ('6', 'blue')]
+
+    >>> coloring_decision({'1': ['2', '3','5', '4','6'], '2': ['1', '5', '3'], '3': \
+    ['2', '6', '4','1'], '4': ['3', '6', '1'], '5': ['2', '1', '6'], '6': ['1','5', '3', '4']})
+    "This graph can't be colored by three colors"
+    """
+    coloring_dict = {}
+    for vert in graph.keys():
+        coloring_dict[vert] = 0
+    vertices = list(graph.keys())
+
+    if not coloring_rec(coloring_dict, vertices, 0, graph):
+        return "This graph can't be colored by three colors"
+
+    result = []
+    for vert, color in coloring_dict.items():
+        result.append((vert, color))
+    return result
+
+
 if __name__ == '__main__':
     import pandas as pd
     import os.path
@@ -252,7 +332,7 @@ if __name__ == '__main__':
 
 
     #  read information from csv
-    if not os.path.exists(path_file):
+    if os.path.exists(path_file):
         graphs = read_data(path_file)
         not_oriented_graph = graphs[1]
         oriented_graph = graphs[0]
@@ -261,13 +341,16 @@ if __name__ == '__main__':
             print('the graph is not connected')
         else:
             #  our functions
-            print(f'Graph is bipartite - {bipartite(not_oriented_graph)}')
+            print(f'Graph is bipartite: {bipartite(not_oriented_graph)}')
             #  euler
-            print(f'Euler cycle for directed graph - {euler_circuit_directed(read_data(path_file)[0])}')
-            print(f'Euler cycle for not directed graph - {euler_circuit_not_directed(read_data(path_file)[1])}')
+            print(f'Euler cycle for directed graph: {euler_circuit(oriented_graph, True)}')
+            print(f'Euler cycle for not directed graph: {euler_circuit(not_oriented_graph, False)}')
             #  hamilton
             print(f"Hamiltonian Cycle for not connected graph: {pre_hamilton(not_oriented_graph, False)}")
             print(f"Hamiltonian Cycle for connected graph: {pre_hamilton(oriented_graph, True)}")
+            # coloring
+            print(f'Coloring graph by 3 colors: {coloring_decision(not_oriented_graph)}')
+            
     else:
         print("file doesn't exist")
  
